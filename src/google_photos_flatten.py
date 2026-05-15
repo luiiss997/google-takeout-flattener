ef1890f98902d1cafc237844144e1c637453c17b
luiiss997 <luis_atencoa97@outlook.com>
2026-05-14 21:07:29 -0600

    feat: Added order by media
    The files result now are order by media and not divided in fotos/videos. Also added a validation to ignore metadata json files

--- src/google_photos_flatten.py
+++ src/google_photos_flatten.py
@@ -24,8 +24,7 @@ DEST_DIR.mkdir(
 
 known_urls = set()
 
-photo_files = []
-video_files = []
+media_files = []
 
 duplicates = 0
 skipped = 0
@@ -39,6 +38,12 @@ for json_file in SOURCE_DIR.rglob("*.json"):
             encoding="utf-8"
         ) as f:
             data = json.load(f)
+            
+        if "title" not in data:
+            print(
+                f"JSON ignorado: {json_file.name}"
+            )
+            continue    
 
         title = data.get(
             "title"
@@ -76,10 +81,8 @@ for json_file in SOURCE_DIR.rglob("*.json"):
         for candidate in json_file.parent.iterdir():
             if not candidate.is_file():
                 continue
-            
             if candidate.suffix == ".json":
                 continue
-            
             if base_name in candidate.stem:
                 matches.append(candidate)
 
@@ -92,7 +95,7 @@ for json_file in SOURCE_DIR.rglob("*.json"):
 
         # tomar el primero encontrado
         file_path = matches[0]
-
+        
         extension = (
             file_path
             .suffix
@@ -119,41 +122,45 @@ for json_file in SOURCE_DIR.rglob("*.json"):
             date
         )
 
-        if extension in PHOTO_EXTENSIONS:
-            photo_files.append(
-                data_tuple
+        media_files.append(
+            (
+                file_path,
+                date,
+                extension
             )
-        else:
-            video_files.append(
-                data_tuple
-            )
+        )
 
     except Exception as e:
         print(
-            f"Error {json_file.name}"
+            f"Error procesando "
+            f"{json_file.name}"
         )
         print(e)
+        continue
 
 print("Ordenando...")
-photo_files.sort(
-    key=lambda x: x[1]
-)
-video_files.sort(
+media_files.sort(
     key=lambda x: x[1]
 )
 
-photo_counter = 1
+media_counter = 1
 
-for file, date in photo_files:
-    extension = (
-        file.suffix
-        .lower()
-    )
+for file, date, extension in media_files:
+    if not file.exists():
+        print(
+            f"Ya procesado: {file.name}"
+        )
+        continue
+
+    media_type = "IMG"
+    if extension in VIDEO_EXTENSIONS:
+        media_type = "VID"
 
     new_name = (
         f"{BACKUP_PREFIX}"
-        f"_IMG"
-        f"{photo_counter:04d}"
+        f"_MEDIA"
+        f"{media_counter:05d}"
+        f"_{media_type}"
         f"{extension}"
     )
 
@@ -162,60 +169,17 @@ for file, date in photo_files:
         / new_name
     )
 
-    if not file.exists():
-        print(
-            f"Ya procesado: {file.name}"
-        )
-        continue
-
     shutil.move(
         str(file),
         str(destination)
     )
 
     print(
-        f"Foto -> {new_name}"
+        f"{new_name}"
     )
 
     moved += 1
-    photo_counter += 1
-
-video_counter = 1
-
-for file, date in video_files:
-    extension = (
-        file.suffix
-        .lower()
-    )
-    new_name = (
-        f"{BACKUP_PREFIX}"
-        f"_VID"
-        f"{video_counter:04d}"
-        f"{extension}"
-    )
-
-    destination = (
-        DEST_DIR
-        / new_name
-    )
-
-    if not file.exists():
-        print(
-            f"Ya procesado: {file.name}"
-        )
-        continue
-
-    shutil.move(
-        str(file),
-        str(destination)
-    )
-
-    print(
-        f"Video -> {new_name}"
-    )
-
-    moved += 1
-    video_counter += 1
+    media_counter += 1
 
 print("\nLimpiando archivos sobrantes...")
 deleted_files = 0
